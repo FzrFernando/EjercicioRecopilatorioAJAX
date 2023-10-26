9524e7b780dd9e5185037303da51e8791176584e
Fernando Zamora <fernandozamorarico@gmail.com>
2023-10-26 14:27:28 +0200

    Eliminar

--- script.js
+++ script.js
@@ -9,11 +9,33 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Función para agregar un usuario a la lista
-    function addUserToList(user) {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `${user.name} : ${user.address} : ${user.email} : <button class="edit" value="${user.id}">Editar</button> <button class="delete" value="${user.id}">Delete</button>`;
-        userList.appendChild(listItem);
+    function addUserToListID(user) {
+        const xhruser = new XMLHttpRequest();
+        xhruser.open('GET',`http://localhost:3000/users?email=${user.email}`,true);
+        xhruser.send();
+        xhruser.onload = function () {
+            if (xhruser.status === 200) {
+                let tmpUser = JSON.parse(xhruser.responseText)[0];     
+                const listItem = document.createElement("li");
+                listItem.innerHTML = `${tmpUser.name} : ${tmpUser.address} : ${tmpUser.email} : <button class="edit" value="${tmpUser.id}">Editar</button> <button class="delete" value="${tmpUser.id}">Delete</button>`;
+                userList.appendChild(listItem);
+            }
+        }
     }   
+
+
+    function addUserToList(user) {
+        if(user.id){
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `${user.name} : ${user.address} : ${user.email} : <button class="edit" value="${user.id}">Editar</button> <button class="delete" value="${user.id}">Delete</button>`;
+            userList.appendChild(listItem);
+
+        }else{
+            addUserToListID(user)
+        }
+    }   
+
+    
     
     // Obtener la lista de usuarios de la API
     function getUsersFromAPI(){
@@ -42,13 +64,31 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         
         if (newUser.name && newUser.address && validateEmail(newUser.email)) {
-            const peticion = new XMLHttpRequest();
-            peticion.open('POST','http://localhost:3000/users');
-            peticion.setRequestHeader('Content-type','application/json');
-            peticion.send(JSON.stringify(newUser));
-            peticion.addEventListener('load', function() {
-                addUserToList(newUser);
-            })
+            
+            if (userForm.dataset.editing) {
+                
+            }else {
+                const comprobarEmail = new XMLHttpRequest();
+                comprobarEmail.open('GET',`http://localhost:3000/users?email=${newUser.email}`,true);
+                comprobarEmail.onload = function () {
+                    if(comprobarEmail.status === 200) {
+                        const users = JSON.parse(comprobarEmail.responseText);
+                        if (users.length > 0) {
+                            alert("El email ya existe en el servidor")
+                            return;
+                        }
+                        const peticion = new XMLHttpRequest();
+                        peticion.open('POST','http://localhost:3000/users');
+                        peticion.setRequestHeader('Content-type','application/json');
+                        peticion.send(JSON.stringify(newUser));
+                        peticion.addEventListener('load', function() {
+                            addUserToList(newUser);
+                        })
+                    }
+                }
+                comprobarEmail.send();
+            }
+            userForm.reset();
         }
         else {
             alert('Alguno de los campos no es correcto');
@@ -58,16 +98,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // Función para borrar un usuario de la lista
     function deleteUser(event){
         if (event.target.classList.contains("delete")) {
+            const listItem = event.target.parentElement;
             let userId = event.target.getAttribute("value");
             const xhr = new XMLHttpRequest();
             xhr.open('DELETE',`http://localhost:3000/users/${userId}`);
             xhr.send();
+            listItem.remove();
         }
+    }
+
+    // Función para editar un usuario de la lista
+    function editUser(event){
+        if (event.target.classList.contains("edit")) {
+            const listItem = event.target.parentElement;
+            const userArray = listItem.textContent.split(" : ");
+            const name = userArray[0];
+            const address = userArray[1];
+            const email = userArray[2];
+            userForm.elements.name.value = name;
+            userForm.elements.address.value = address;
+            userForm.elements.email.value = email;
+            userForm.dataset.editing = email;
+            userForm.querySelector("button[type='submit']").textContent = "Editar Usuario";
+            userForm.dataset.editingIndex = [...userList.children].indexOf(listItem);
+        }
+
     }
 
     // Manejar clics en botones de borrar y editar
     userList.addEventListener("click", function (event) {
         deleteUser(event);
-        // editUser(event);
+        editUser(event);
     });
 });
